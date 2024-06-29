475d72fef0aad271d449ee0e2c0d6d1085aacf7c
RaghulMK03 <mailtooraghul@gmail.com>
2024-06-29 22:23:26 +0530

    Add files via upload

---
+++ QR- Code  Generator/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import QrCodeGen from './QrCodeGen.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    
+    <QrCodeGen />
+  </React.StrictMode>,
+)
