8452e58ce1b6489750bdbb8c48285e0ecbdcd81f
RaghulMK03 <mailtooraghul@gmail.com>
2024-06-29 21:15:22 +0530

    Add files via upload

---
+++ Strong Password/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css';
+import PasswordGenerator from './components/PasswordGenerator';
+
+
+function App ()  {
+  
+  return (
+    <>
+    <div className="digital-clock">
+      <PasswordGenerator />
+      
+
+      </div>
+    </>
+    
+  )
+}
+
+export default App
---
+++ Strong Password/src/components/PasswordGenerator.jsx
@@ -0,0 +1,75 @@
+import React,{useState} from 'react'
+
+export const PasswordGenerator = () => {
+    const [length,setLength] = useState(8);
+    const [includeUpperCase,setIncludeUpperCase] = useState(true);
+    const [includeLowerCase,setIncludeLowerCase] = useState(true);
+    const [includeNumbers,setIncludeNumbers] = useState(true);
+    const [includeSymbols,setIncludeSymbols] = useState(true);
+    const [password,setPassword] =useState("");
+
+    const generatePassword = () => {
+        let charset="";
+        if(includeUpperCase) charset +="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        if(includeLowerCase) charset +="abcdefghijklmnopqrstuvwxyz";
+        if(includeNumbers) charset+="0123456789";
+        if(includeSymbols) charset +="!@#$%^&*()_-+=";
+        // console.log(charset);
+
+        let generatePassword ="";
+        for(let i = 0; i < length ; i++){
+            const randomIndex =Math.floor(Math.random() * charset.length);
+            generatePassword += charset[randomIndex];
+        }
+
+        setPassword(generatePassword);
+    };
+
+    const copyToCliboard = () => {
+        navigator.clipboard.writeText(password);
+        alert("Password Copied")
+    }
+  return (
+    <div className="password-generator">
+        <h2>Strong Password Generator</h2>
+        <div className="input-group">
+            <label htmlFor="num">Password Length : </label>
+            <input type="number" id="num"  value={length} 
+            onChange={(e) => setLength(parseInt(e.target.value))}/>
+        </div>
+        <div className="checkbox-group">
+            <input type="checkbox"  id="upper"  checked={includeUpperCase}
+            onChange={(e) => setIncludeUpperCase(e.target.checked)}/>
+            <label htmlFor="upper">Include Uppercase</label>
+
+        </div>
+        <div className="checkbox-group">
+            <input type="checkbox"  id="lower"checked={includeLowerCase} 
+             onChange={(e) => setIncludeLowerCase(e.target.checked)}/>
+            <label htmlFor="lower">Include lowercase</label>
+
+        </div>
+        <div className="checkbox-group">
+            <input type="checkbox"  id="numbers" checked={includeNumbers}
+            onChange={(e) => setIncludeNumbers(e.target.checked)}/>
+            <label htmlFor="numbers"> Include Numbers</label>
+
+        </div>
+        <div className="checkbox-group">
+            <input type="checkbox"  id="symbol" checked={includeSymbols}
+             onChange={(e) => setIncludeSymbols(e.target.checked)}/>
+            <label htmlFor="symbol"> Include Symbol</label>
+
+        </div>
+        <button className="generate-btn" onClick={generatePassword}>
+            Generate Password
+        </button>
+        <div className="generated-password">
+            <input type="text" readOnly  value={password}/>
+            <button className='copy-btn' onClick={copyToCliboard}>COPY</button>
+        </div>
+    </div>
+  )
+}
+
+export default PasswordGenerator
