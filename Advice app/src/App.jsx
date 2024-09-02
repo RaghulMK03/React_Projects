5eb0977bff32c7aad641c532bbd93e533a3d9e27
RaghulMK03 <mailtooraghul@gmail.com>
2024-09-02 21:57:38 +0530

    Add files via upload
    Modified file

---
+++ Advice app/src/App.jsx
@@ -0,0 +1,14 @@
+import AdviceApp from './formSampple/AdviceApp'
+import './index.css'
+
+function App() {
+  
+  return (
+    <div>
+      <AdviceApp />
+
+    </div>
+  )
+}
+
+export default App
